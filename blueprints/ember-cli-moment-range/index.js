6d01cf5fa0c3e757d0f1395dc3daecc075f595b6
Hank Fanchiu <fanchiu.hank@gmail.com>
2016-05-18 17:43:46 -0700

    Clean up package.json, bower.json, blueprints, and travis.yml

--- blueprints/ember-cli-moment-range/index.js
+++ blueprints/ember-cli-moment-range/index.js
@@ -1,8 +1,6 @@
 /*jshint node:true*/
 module.exports = {
-  normalizeEntityName: function () {
-    // this prevents an error when the entityName is not specified (since that doesn't actually matter to us)
-  },
+  normalizeEntityName: function () {},
 
   afterInstall: function () {
     return this.addBowerPackageToProject('moment-range', '^2.2.0');
